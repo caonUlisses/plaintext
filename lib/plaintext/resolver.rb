ec50a61f25202ee425d6f28b76097a7aab00f5a0
Ulisses Caon <ulissescaon@gmail.com>
2026-02-20 13:12:05 -0300

    Remove deprecation warning

--- lib/plaintext/resolver.rb
+++ lib/plaintext/resolver.rb
@@ -2,7 +2,6 @@
 
 module Plaintext
   class Resolver
-
     # maximum length of returned plain text in bytes. Default: 4MB
     attr_accessor :max_plaintext_bytes
 
@@ -10,17 +9,18 @@ module Plaintext
       attr_accessor :cached_file_handlers
 
       HANDLERS = [
-          Plaintext::PdfHandler,
-          Plaintext::OpendocumentHandler,
-          Plaintext::DocxHandler, Plaintext::XlsxHandler, Plaintext::PptxHandler,
-          Plaintext::DocHandler, Plaintext::XlsHandler, Plaintext::PptHandler,
-          Plaintext::ImageHandler,
-          Plaintext::RtfHandler,
-          Plaintext::PlaintextHandler
+        Plaintext::PdfHandler,
+        Plaintext::OpendocumentHandler,
+        Plaintext::DocxHandler, Plaintext::XlsxHandler, Plaintext::PptxHandler,
+        Plaintext::DocHandler, Plaintext::XlsHandler, Plaintext::PptHandler,
+        Plaintext::ImageHandler,
+        Plaintext::RtfHandler,
+        Plaintext::PlaintextHandler
       ].freeze
 
       def file_handlers
-        return self.cached_file_handlers if self.cached_file_handlers.present?
+        return cached_file_handlers if cached_file_handlers.present?
+
         self.cached_file_handlers = HANDLERS.map(&:new)
       end
     end
@@ -31,17 +31,16 @@ module Plaintext
       @max_plaintext_bytes = 4_194_304 # 4 megabytes
     end
 
-
     # Returns the extracted fulltext or nil if no matching handler was found
     # for the file type.
     def text
       if handler = find_handler and
-          text = handler.text(@file, max_size: max_plaintext_bytes)
+         text = handler.text(@file, max_size: max_plaintext_bytes)
 
         text = +text
         text.gsub!(/\s+/m, ' ')
         text.strip!
-        text.unicode_normalize(:nfc).byteslice(0, max_plaintext_bytes)
+        text.each_char.map(&:unicode_normalize).join[0, max_plaintext_bytes]
       end
     end
 
@@ -50,6 +49,5 @@ module Plaintext
     def find_handler
       self.class.file_handlers.detect { |h| h.accept? @content_type }
     end
-
   end
 end
